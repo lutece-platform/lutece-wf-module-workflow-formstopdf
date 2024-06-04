6129972cdd518fdedc2f474abaa8f8383627e89f
TimotheeHrl <tharleaux@gmail.com>
2024-06-04 11:08:39 +0200

    LUT-28077 : Error in sql script

--- src/sql/plugins/workflow/modules/formspdf/upgrade/update_db_workflow-formspdf-1.0.0-1.0.1.sql
+++ src/sql/plugins/workflow/modules/formspdf/upgrade/update_db_workflow-formspdf-1.0.0-1.0.1.sql
@@ -1,3 +1,3 @@
 ALTER TABLE workflow_task_formspdf_cf DROP COLUMN template;
 ALTER TABLE workflow_task_formspdf_cf ADD COLUMN id_template INT(11) NOT NULL DEFAULT '0' AFTER format;
-ALTER TABLE workflow_task_formspdf_template ADD COLUMN is_rte SMALLINT NOT NULL DEFAULT 0;
+ALTER TABLE workflow_task_formspdf_template ADD COLUMN is_rte SMALLINT NOT NULL DEFAULT '0';
