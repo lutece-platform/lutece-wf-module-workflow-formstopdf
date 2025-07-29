6499c7ccb9d798daf7d552726763fb3426ff5515
seboo <25958061+seboo@users.noreply.github.com>
2025-07-29 16:33:59 +0200

    update global pom and add default test

---
+++ src/test/java/fr/paris/lutece/plugins/workflow/modules/formspdf/business/testFormsToPdf.java
@@ -0,0 +1,13 @@
+package fr.paris.lutece.plugins.workflow.modules.formspdf.business;
+
+
+import fr.paris.lutece.test.LuteceTestCase;
+
+public class testFormsToPdf extends LuteceTestCase 
+{
+	 public void testIsActiveWhenActivationIsTrue( )
+	    {
+		 	// default test for CI
+	        assert( true );
+	    }
+}
