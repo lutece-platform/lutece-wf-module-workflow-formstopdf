5bb2759273bf36e4aae1b9a808bac26543509c78
jlf-eviden <jlf-eviden@DESKTOP-BCRT9V4>
2025-08-26 17:55:48 +0200

    LUT-30623 : Tables not created in HSQLDB (V8)

--- src/sql/plugins/workflow/modules/formspdf/upgrade/update_db_workflow-formspdf-1.0.0-1.0.1.sql
+++ src/sql/plugins/workflow/modules/formspdf/upgrade/update_db_workflow-formspdf-1.0.0-1.0.1.sql
@@ -1,6 +1,9 @@
 -- liquibase formatted sql
 -- changeset workflow-formspdf:update_db_workflow-formspdf-1.0.0-1.0.1.sql
 -- preconditions onFail:MARK_RAN onError:WARN
-ALTER TABLE workflow_task_formspdf_cf DROP COLUMN template;
-ALTER TABLE workflow_task_formspdf_cf ADD COLUMN id_template INT(11) NOT NULL DEFAULT '0' AFTER format;
-ALTER TABLE workflow_task_formspdf_template ADD COLUMN is_rte SMALLINT NOT NULL DEFAULT '0';
+ALTER TABLE workflow_task_formspdf_cf MODIFY COLUMN id_task INT DEFAULT '0' NOT NULL;
+ALTER TABLE workflow_task_formspdf_cf MODIFY COLUMN id_forms INT DEFAULT '0' NULL;
+ALTER TABLE workflow_task_formspdf_cf MODIFY COLUMN id_template INT DEFAULT '0' NOT NULL;
+
+ALTER TABLE workflow_task_formspdf_template MODIFY COLUMN id_form INT DEFAULT -1 NOT NULL;
+ALTER TABLE workflow_task_formspdf_template MODIFY COLUMN content LONG VARCHAR;
