9eb96f5f83181f77d89d335bc34f332090203c66
ryahiaoui <ryahiaoui@users.noreply.github.com>
2026-09-01 11:53:43 +0200

    LUT-33288: Complete the plugin rename : SQL directory, PLUGIN_NAME, i18n keys and data migration

--- src/sql/plugins/workflow/modules/formstopdf/upgrade/update_db_workflow-formspdf-1.0.1-1.0.2.sql
+++ src/sql/plugins/workflow/modules/formstopdf/upgrade/update_db_workflow-formspdf-1.0.1-1.0.2.sql
@@ -1,4 +1,4 @@
 -- liquibase formatted sql
--- changeset workflow-formspdf:update_db_workflow-formspdf-1.0.1-1.0.2.sql
+-- changeset workflow-formspdf:update_db_workflow-formspdf-1.0.1-1.0.2.sql logicalFilePath:sql/plugins/workflow/modules/formspdf/upgrade/update_db_workflow-formspdf-1.0.1-1.0.2.sql
 -- preconditions onFail:MARK_RAN onError:WARN
 ALTER TABLE workflow_task_formspdf_template ADD COLUMN is_replace_empty_response SMALLINT DEFAULT 0 NOT NULL;
